7a0f3668167e46bff7b18d29bdfbc086decf1ec6
Isabella Flórez <isa11cano28@gmail.com>
2025-11-12 14:28:06 -0500

    Update gimnasio1.py

---
+++ gimnasio1.py
@@ -0,0 +1,8 @@
+# Repetir mensaje de motivación 5 veces con while
+contador = 1
+while contador <= 5:
+    if contador == 5:
+        print("¡Excelente trabajo, terminaste!")
+    else:
+        print("¡Tú puedes lograrlo!")
+    contador += 1
