b2a9b66a902ce46eb8d237d316f2a88b3fb5bbd6
Isabella Flórez <isa11cano28@gmail.com>
2025-11-12 18:35:59 -0500

    aprendemas.py

---
+++ aprendemas.py
@@ -0,0 +1,14 @@
+def promedio_notas()
+    while estudiante:
+        nnom_estu = input("ingrese su nombre (para salir ingrese 1)")
+        if nnom_estu = 1
+            break
+        elif
+            nota1 = float(input("ingrese la primera nota: "))
+            nota2 = float(input("ingrese la segunda nota:"))
+            nota3 = float(input("ingrese la tercera nota:"))
+            promedio = (nota1 + nota2 + nota3) / 3
+            if promedio >= 3:
+            print("aprobado")
+            else:
+            print("reprobado")
