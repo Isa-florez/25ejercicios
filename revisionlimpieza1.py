620a4b78511a0b7e6b4814a2166fe6b8f6db4823
Isabella Flórez <isa11cano28@gmail.com>
2025-11-12 14:26:44 -0500

    revisionlimpieza1.py

---
+++ revisionlimpieza1.py
@@ -0,0 +1,8 @@
+# repetir 3 veces el mensaje “Limpia tu estación”.
+# Si es la última vez, mostrar “¡Revisión completada!”.
+print ("bienvenido a tu revision de limpieza")
+
+for i in range(1,4):
+    print("limpia tu estacion")
+    if i == 3:
+        print ("¡revision completada!")
